36c8345b94cb07242905e283e0f7283a468e1d34
carlos <chcruz64@gmail.com>
2022-07-15 08:22:01 -0300

    up

--- cadastro_usuario/frontend/src/main/routes.jsx
+++ cadastro_usuario/frontend/src/main/routes.jsx
@@ -1,13 +1,15 @@
-import React from "react"; 
+import React from "react";
 import { Routes, Route } from "react-router-dom";
+ 
 import Home from "../components/home/home";
-import Usercrud from "../components/user/usercrud";
-
+import UserCrud from "../components/user/usercrud";
+ 
 export default props => 
-    <Routes>
-        <Route exact path="/" component={<Home />} />
-        <Route path="/users" component={<Usercrud />} />
-        <Route from="*" to={<Home />} />
-    </Routes>
+  <Routes>
+    <Route exact path="/" element={<Home />} />
+    <Route path="/users" element={<UserCrud />} />
+    <Route path="*" element={<Home />} />
+  </Routes>
+
 
 
